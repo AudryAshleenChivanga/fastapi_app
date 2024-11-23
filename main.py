c9b6f2408652415f8aedae5e9a3903049bbe3080
AudryAshleenChivanga <a.chivanga@alustudent.com>
2024-11-23 20:41:36 +0200

    Update

---
+++ main.py
@@ -0,0 +1,21 @@
+iimport pandas as pd
+import pickle
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class PredictionInput(BaseModel):
+    age: int
+    sex: int
+    bmi: float
+    children: int
+    smoker: int
+    region: int
+
+def load_model():
+    with open('best_model.pkl', 'rb') as file:
+        model = pickle.load(file)
+    return model
+
+model = load_model()
